65596874f0020be4279c709a97836079f384f005
shuyun Chen <1072751839@qq.com>
2018-10-30 16:03:39 +0800

    login样式修改
    验证码

--- lesson 3/simple-login/src/components/Login.vue
+++ lesson 3/simple-login/src/components/Login.vue
@@ -1,32 +1,38 @@
 <template>
-  <div class="content">
-    <div class="title">
-      <label>绑定账号</label>
-    </div>
-    <div>
+  <div>
+    <div class="content">
+      <div class="title">
+        <label>Hi<br>欢迎加入</label>
+      </div>
+      <Sms v-model="code"></Sms>
+        <!-- 手机号 begin-->
       <div  class="input">
         <div class="inputTitle">
           <label v-show="isUserNameExist">手机号</label>
         </div>
-        <input type="text" v-model="userName" placeholder="请输入手机号"/>
+        <input type="number" @input="verifyUserName" v-model="userName" placeholder="请输入手机号"/>
       </div>
+        <!-- 手机号 end-->
+        <!-- 密码 begin-->
       <div  class="input">
         <div class="inputTitle">
           <label v-show="isPasswordExist">密码</label>
         </div>
-        <input type="password" v-model="password" placeholder="请输入密码"/>
+        <input type="password" @input="verifyPassword" v-model="password" placeholder="请输入密码"/>
       </div>
-      <div>
-        <button type="button" class="bindButton">绑定</button>
-      </div>
-      <div class="changeLogin" @click="driverLogin">司机账号登录</div>
-    </div>
-
+        <!-- 密码 end-->
+        <div align="center">
+          <button type="button" class="bindButton" >登录</button>
+        </div>         
+     </div>
+     <!-- <div class="changeLogin" @click="driverLogin">司机账号登录</div>   -->
+    
+     <div class="changeLogin">第三方账号登录</div>  
   </div>
 </template>
 
 <script>
-
+  import Sms from 'ofcold-security-code'
   import { TopTips } from 'we-vue'
 
     export default {
@@ -35,6 +41,7 @@
           return {
             userName: "",
             password: "",
+            code: ''
           }
         },
         computed: {
@@ -49,16 +56,26 @@
         methods: {
           driverLogin() {
             this.$router.push("DriverLogin")
+          },
+          verifyUserName() {
+            if (this.userName.length > 11) {
+              TopTips({ message: '请正确输入手机号', duration: 2000})
+            } else if (this.userName.length == 0) {
+              TopTips({ message: '手机号不能为空', duration: 2000})
+            }
           }
-        }
+        },
+        components: {
+          Sms
+        }       
     }
 </script>
 
-<style scoped>
+<style>
   .content {
-    margin: 60px 30px 4px 30px;
-    text-align: left;
-  }
+      margin: 60px 30px;
+      text-align: left;
+    }
 
   .title {
     font-size: 24px;
@@ -66,43 +83,71 @@
   }
 
   .input {
-    border-bottom: 1px solid rgba(235,238,245,1);
+    border-bottom: 1px solid #f0f0f0;
     margin-top: 26px;
   }
 
   .input label {
-    color: #C0C4CC;
+    color: #C7C7CC;
     font-size: 12px;
   }
 
   .input .inputTitle {
-    height: 16px;
+    height: 18px;
   }
 
   .input input {
-    color: #303133;
+    color: #1d1d26;
     margin: 12px 0;
     line-height: 22px;
     width: 100%;
-    font-size: 16px;
+    font-size: 18px;
     outline: 0;
     border-width: 0;
   }
 
   .bindButton {
     margin-top: 58px;
-    height: 40px;
-    width: 100%;
-    background-color: #000099;
+    padding: 10px 0;
+    width: 80%;
+    background-color: #FF7F41;
     color: #FFF;
     font-size: 16px;
-    border-radius: 4px;
+    border-radius: 20px;
     outline: 0;
+    border:0;
+  }
+
+  .bindButton:hover {
+    background-color: #E67339;
   }
 
   .changeLogin {
-    text-align: left;
-    color: #000099;
+    position: absolute;
+    bottom:30px;
+    width: 100%;
+    color: #FF7F41;
+    font-size: 12px;
+  }
+  
+  .changeLogin {
+    position: absolute;
+    bottom:30px;
+    width: 100%;
+    color: #FF7F41;
+    font-size: 12px;
+    background-color: #FFF;
   }
 
+  .ofcold__security-code-wrapper .ofcold__security-code-field{
+      margin-right: 10px!important;
+  }
+
+  .ofcold__security-code-wrapper .ofcold__security-code-field .form-control{
+      border: 1px solid #c7c7c7;
+  }
+
+  .ofcold__security-code-wrapper .ofcold__security-code-field:nth-child(3) {
+      margin-right: 10px!important;
+  }
 </style>
