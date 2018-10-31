2a490a617cd1f8006f8aa3c862696d3f0f350974
shuyun Chen <1072751839@qq.com>
2018-10-31 10:57:41 +0800

    css样式表合并
    合并样式表，修改登录页样式

--- lesson 3/simple-login/src/components/Login.vue
+++ lesson 3/simple-login/src/components/Login.vue
@@ -13,21 +13,26 @@
         <input type="number" @input="verifyUserName" v-model="userName" placeholder="请输入手机号"/>
       </div>
         <!-- 手机号 end-->
-        <!-- 密码 begin-->
+        <!-- 密码 begin
       <div  class="input">
         <div class="inputTitle">
           <label v-show="isPasswordExist">密码</label>
         </div>
-        <input type="password" @input="verifyPassword" v-model="password" placeholder="请输入密码"/>
+        <input type="password" v-model="password" placeholder="请输入密码"/>
       </div>
-        <!-- 密码 end-->
+        密码 end-->
         <div align="center">
-          <button type="button" class="bindButton" >登录</button>
+          <button type="button" class="bindButton" >获取验证码</button>
         </div>         
      </div>
      <!-- <div class="changeLogin" @click="driverLogin">司机账号登录</div>   -->
-    
-     <div class="changeLogin">第三方账号登录</div>  
+      <div class="thirdNum">
+        <span class="accountText">第三方账号登录</span>
+        <ul>
+          <li><i class="alipay-login"></i></li>
+          <li><i class="wechat-login"></i></li>
+        </ul>
+      </div>
   </div>
 </template>
 
@@ -72,74 +77,10 @@
 </script>
 
 <style>
-  .content {
-      margin: 60px 30px;
-      text-align: left;
-    }
+  @import '../styles/main.css';
+  @import '../styles/login.css';
 
-  .title {
-    font-size: 24px;
-    color: #303133;
-  }
-
-  .input {
-    border-bottom: 1px solid #f0f0f0;
-    margin-top: 26px;
-  }
-
-  .input label {
-    color: #C7C7CC;
-    font-size: 12px;
-  }
-
-  .input .inputTitle {
-    height: 18px;
-  }
-
-  .input input {
-    color: #1d1d26;
-    margin: 12px 0;
-    line-height: 22px;
-    width: 100%;
-    font-size: 18px;
-    outline: 0;
-    border-width: 0;
-  }
-
-  .bindButton {
-    margin-top: 58px;
-    padding: 10px 0;
-    width: 80%;
-    background-color: #FF7F41;
-    color: #FFF;
-    font-size: 16px;
-    border-radius: 20px;
-    outline: 0;
-    border:0;
-  }
-
-  .bindButton:hover {
-    background-color: #E67339;
-  }
-
-  .changeLogin {
-    position: absolute;
-    bottom:30px;
-    width: 100%;
-    color: #FF7F41;
-    font-size: 12px;
-  }
-  
-  .changeLogin {
-    position: absolute;
-    bottom:30px;
-    width: 100%;
-    color: #FF7F41;
-    font-size: 12px;
-    background-color: #FFF;
-  }
-
-  .ofcold__security-code-wrapper .ofcold__security-code-field{
+    .ofcold__security-code-wrapper .ofcold__security-code-field{
       margin-right: 10px!important;
   }
 
@@ -150,4 +91,5 @@
   .ofcold__security-code-wrapper .ofcold__security-code-field:nth-child(3) {
       margin-right: 10px!important;
   }
+
 </style>
